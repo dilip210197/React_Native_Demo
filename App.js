b48a3b8478d773694c162366666e61fef7480857
dilip210197 <89332489+dilip210197@users.noreply.github.com>
2026-04-03 20:37:25 +0530

    Initialize App component with navigation

---
+++ App.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './TabContainer'; // The file we created earlier
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    
+      <TabNavigation />
+      
+    </NavigationContainer>
+  );
+}
