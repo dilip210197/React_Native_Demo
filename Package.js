cf1adfe5f06ae0b4bc8fbc77ebab47c50532f0c3
dilip210197 <89332489+dilip210197@users.noreply.github.com>
2026-04-03 20:38:27 +0530

    Add Package.js with project dependencies

---
+++ Package.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": {
+    "react-native-paper": "4.9.2",
+    "@expo/vector-icons": "^15.0.3",
+    "@react-navigation/native": "7.2.2",
+    "@react-navigation/bottom-tabs": "",
+    "react-native-screens": "~4.16.0",
+    "react-native-safe-area-context": "~5.6.0"
+  }
+}
